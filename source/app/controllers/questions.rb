bd956ad2260ee2939dddb06957343aa229d81319
chris zajac & Harrison Klucher & Nicholas Christiny & Nic Lombardi <pair+cMzeta.hklucher.nchristiny.nilo916@devbootcamp.com>
2015-09-17 16:03:11 -0500

    add tag searching functionality

--- source/app/controllers/questions.rb
+++ source/app/controllers/questions.rb
@@ -10,10 +10,26 @@ end
 post '/questions' do
 	user = User.find_by(id: session[:user_id])
 	@question = Question.new(user_id: user.id, content: params[:question_content])
-  redirect to '/questions/#{@question.id}'
+  redirect to "/questions/#{@question.id}"
+end
+
+get '/questions/search' do
+  erb :"questions/search"
+end
+
+get '/questions/search/:query' do
+  @query = params[:query]
+  tag = Tag.find_by(name: @query)
+  searched_question_id = tag.question_id
+  @questions = Question.where({ id: searched_question_id })
+  erb :'/questions/query'
 end
 
 get '/questions/:id' do
   @question = Question.find(params[:id])
   erb :'/questions/show'
+end
+
+post '/questions/:query' do
+  redirect "/questions/search/#{params[:params][:query]}"
 end
