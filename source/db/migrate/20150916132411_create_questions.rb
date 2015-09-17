c170d91f73e542537e4354eac44de52ad30a7505
chris zajac & Harrison Klucher & Nicholas Christiny & Nic Lombardi <pair+cMzeta.hklucher.nchristiny.nilo916@devbootcamp.com>
2015-09-17 13:44:32 -0500

    add title column to the question table

--- source/db/migrate/20150916132411_create_questions.rb
+++ source/db/migrate/20150916132411_create_questions.rb
@@ -2,9 +2,10 @@ class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
       t.text :content, null: false
+      t.string :title, null: false 
       t.integer :user_id, null: false
 
-      t.timestamps null: false 
+      t.timestamps null: false
     end
   end
 end
